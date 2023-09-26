d668deb125e931ee875618af76823130234e4b12
Sharifia <el.sh200@gmail.com>
2023-09-26 18:10:42 +0330

    Add static and media setting

--- healitia/settings.py
+++ healitia/settings.py
@@ -123,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles' # For Deployment
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
